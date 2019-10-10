4f451a8780ff785fc8f18ff1299555938eeb1fcd
benjamin <benjamin.dartigues@u-bordeaux.fr>
2019-10-10 11:20:24 +0200

    message

--- start_angular_server.sh
+++ start_angular_server.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 
 screen ng serve --port 4200 --host 0.0.0.0 --disableHostCheck --base-href /linda/ --deploy-url /linda/ --public-host https://services.cbib.u-bordeaux.fr/linda/
