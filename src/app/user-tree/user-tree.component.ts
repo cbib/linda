6a6280c05de00e8c7c5493a0a22193e9321f2b42
benjamin <benjamin.dartigues@u-bordeaux.fr>
2019-10-16 14:00:23 +0200

    test

--- src/app/user-tree/user-tree.component.ts
+++ src/app/user-tree/user-tree.component.ts
@@ -32,8 +32,8 @@ interface ExampleFlatNode {
 export class UserTreeComponent implements OnInit{
     //@ViewChild(MatMenuTrigger) 
     //@ViewChild(MatMenuTrigger,{static:true })
-    //@ViewChild('contextMenu',{static:false }) contextMenu: MatMenuTrigger;
-    @ViewChild(MatMenuTrigger) contextMenu: MatMenuTrigger;
+    @ViewChild(MatMenuTrigger,{static:false }) contextMenu: MatMenuTrigger;
+    //@ViewChild(MatMenuTrigger) contextMenu: MatMenuTrigger;
     
     
     disableSelect = new FormControl(false);
