64a5d4819fdbcfef2d0537e2b1606551925f6cf4
benjamin <benjamin.dartigues@u-bordeaux.fr>
2020-05-11 11:42:53 +0200

    remove mail

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,8 +2,8 @@ export class Constants
 {
     static APIConfig = class
     {
-        static APIUrl:string = 'http://127.0.0.1:8529/_db/MIAPPE_GRAPH/xeml/';
-        static get_api(){
+        static APIUrl:string = 'https://services.cbib.u-bordeaux.fr/lindaAPI/_db/MIAPPE_GRAPH/xeml/';
+	static get_api(){
             return this.APIUrl;
         }
     };
