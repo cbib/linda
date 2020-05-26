d960070f9f1e79e0eaf586206ab624a7ee2dd56f
benjamin <benjamin.dartigues@u-bordeaux.fr>
2020-05-26 09:47:10 +0200

    refundation

---
+++ src/app/documentation/ontology-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ontology-description',
+  templateUrl: './ontology-description.component.html',
+  styleUrls: ['./ontology-description.component.css']
+})
+export class OntologyDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
