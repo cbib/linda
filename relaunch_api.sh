231dd2e318b18007c0cbf7edfd3e7419876f9989
bdartigues <bdartigues@gmail.com>
2020-07-15 14:40:47 +0200

    cleaning constants on dev branch

--- relaunch_api.sh
+++ relaunch_api.sh
@@ -2,5 +2,5 @@
 
 rm -rf XEMLAPI.zip
 zip -r XEMLAPI.zip XEMLAPI
-foxx replace /xeml --server http://127.0.0.1:8529 --database MIAPPE_GRAPH -u root -P XEMLAPI.zip
+foxx replace /xeml --server http://10.71.10.106:8529 --database MIAPPE_GRAPH -u root -P XEMLAPI.zip
 
