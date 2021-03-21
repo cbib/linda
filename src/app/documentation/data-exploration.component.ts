b5a2d62032bfd10876e1ef71a6cbb8a644d1a843
benjamin <benjamin.dartigues@u-bordeaux.fr>
2021-03-21 11:43:47 +0100

    add various images and annyang js library

---
+++ src/app/documentation/data-exploration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-exploration',
+  templateUrl: './data-exploration.component.html',
+  styleUrls: ['./data-exploration.component.css']
+})
+export class DataExplorationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/documentation/miappe_components/investigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-investigation',
+  templateUrl: './investigation.component.html',
+  styleUrls: ['./investigation.component.css']
+})
+export class InvestigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
