ec1f7afd473d83193a61aa54209e1f5e03286505
benjamin <benjamin.dartigues@u-bordeaux.fr>
2021-03-22 17:30:36 +0100

    Finalize home page

---
+++ src/app/documentation/miappe_components/study.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-study',
+  templateUrl: './study.component.html',
+  styleUrls: ['./study.component.css']
+})
+export class StudyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
