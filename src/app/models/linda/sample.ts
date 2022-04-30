e8eaf433b7296610eacff07936da26e3bdab20e4
bdartigues <bdartigues@gmail.com>
2022-04-30 20:25:39 +0200

    add complete new sample handler page - link design to samples - replace all pagination table components

---
+++ src/app/models/linda/sample.ts
@@ -0,0 +1,40 @@
+export interface SampleInterface {
+    '_id':string;
+    '_key':string;
+    '_rev':string;
+    'Sample ID':string;
+    'External ID':string;
+    'Plant anatomical entity': string;
+    'Plant structure development stage':string;
+    'Sample description':string;
+    'Collection date':Date;
+    'bmUUID':string;
+    'obsUUID':string;
+    'sampleUUID':string;
+}
+
+export class Sample implements SampleInterface {
+    '_id':string;
+    '_key':string;
+    '_rev':string;
+    'Sample ID':string;
+    'External ID':string;
+    'Plant anatomical entity': string;
+    'Plant structure development stage':string;
+    'Sample description':string;
+    'Collection date': Date;
+    'bmUUID':string;
+    'obsUUID':string;
+    'sampleUUID':string;
+    constructor(sample_id:string,sample_ext_id:string,pae:string,psds:string,sample_desc:string,coll_date:Date,bmuuid:string,obsuuid:string,sampleuuid:string){
+        this["Sample ID"]=sample_id
+        this["External ID"]=sample_ext_id
+        this["Plant anatomical entity"]=pae
+        this["Plant structure development stage"]=psds
+        this["Sample description"]=sample_desc
+        this["Collection date"]=coll_date
+        this["bmUUID"]=bmuuid
+        this["obsUUID"]=obsuuid
+        this["sampleUUID"]=sampleuuid
+    }
+}
