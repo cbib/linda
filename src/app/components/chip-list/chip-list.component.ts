03716456c1e62a809dd682eb4046ac7fcd551f65
bdartigues <bdartigues@gmail.com>
2022-05-13 11:00:45 +0200

    add favicon for navigator tab

---
+++ src/app/components/chip-list/chip-list.component.ts
@@ -0,0 +1,16 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'chip-list',
+  templateUrl: 'chip-list.component.html',
+  styleUrls: ['chip-list.component.css'],
+})
+export class ChipListComponent {
+    @Input() skills;
+    @Output() notify: EventEmitter<{}> = new EventEmitter<{}>();
+    removable=true
+    onRemove(name:string){
+      this.skills=this.skills.filter(skill=>skill.name!==name)
+      this.notify.emit(this.skills)
+    }
+}
