f19d68fc40fbe76a2a9456ff5490c916900e7566
bdartigues <bdartigues@gmail.com>
2022-07-03 19:24:46 +0200

    add new event file for ISA export

--- src/app/modules/application/projects/pages/experimental-design-page.component.ts
+++ src/app/modules/application/projects/pages/experimental-design-page.component.ts
@@ -741,7 +741,7 @@ export class ExperimentalDesignPageComponent implements OnInit, OnDestroy, After
       }
     onBlockIndexChange(value){
         console.log(value)
-        this.block_index = value
+        this.block_index = value -1 
     }
     get get_block_index(){
         return this.block_index
